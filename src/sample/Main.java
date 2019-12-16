bb02fb5e4cd6b4336b5f57a87d7807e07f99293c
s <ledinhanhtan@gmail.com>
2019-12-16 22:10:26 +0700

    Initialize

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/view/DHBC.fxml"));
+        primaryStage.setTitle("DHBC");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
